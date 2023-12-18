e049fd5dfdf52ace7d11f1194ac484e1eeabd29a
sol10amor <hetaretaiyou1013@gmail.com>
2023-12-18 16:23:34 +0900

    46行目のエラー未解決

---
+++ algo5_KeyDeletion.php
@@ -0,0 +1,55 @@
+<?php
+
+//5 文字列と文字の配列を受け取り、文字列から文字の配列に含まれる文字を削除した文字列を返す
+//  （例："OBHTQABQNIB SQHBOHBEBIQ", ['B', 'Q'] →"OHTANI SHOHEI"）
+
+echo "文字列を入力してください\n";
+$str = fgets(STDIN);
+var_dump($str);
+
+echo "消去する文字を入力してください\n";
+$deleteWord = trim(fgets(STDIN));
+var_dump($deleteWord);
+
+
+function converter($deleteWord)
+{
+    $array = [];
+    for($i = 0; $i < countLen($deleteWord); $i++) {
+        $array[] = $deleteWord[$i];
+    }
+    return $array;
+}
+
+function countLen($str)
+{
+    $lenCounter = 0;
+    while($str[$lenCounter] != null) {
+        $lenCounter++;
+    }
+    return $lenCounter;
+}
+
+$deleteWordConvertedToArray = converter($deleteWord);
+
+echo "文字列は配列に変換されました\n";
+var_dump($deleteWordConvertedToArray);
+
+
+
+function deleteWord($str, $deleteWordConvertedToArray)
+{
+    $result = "";
+    for($i = 0 ; $i < countLen($str) ; $i++) {
+        foreach($deleteWordConvertedToArray as $key) {
+            if($str[$i] == $key) {
+                $str[$i] = ' ';      //“”カラ文字に変えるとエラーになる
+            }
+        }
+        $result .= $str[$i];
+    }
+    return $result;
+}
+
+$delResult = deleteWord($str, $deleteWordConvertedToArray);
+var_dump($delResult);
